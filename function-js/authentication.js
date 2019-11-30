4c0c0a654593d1b545418efd54b4213b660cef42
Pisut-1037 <pisut.peter@mail.kmutt.ac.th>
2019-11-30 14:00:30 +0700

    can manage cookie not much

--- function-js/authentication.js
+++ function-js/authentication.js
@@ -1,72 +1,69 @@
-// number of days to expired
-var expDays = 5; 
+$(document).ready(function() {
+    // number of days to expired
+    var exdays = 5;
 
-// ฟังก์ชันเรียกใช้ฟังก์ชันอื่นตามปุ่มที่กด
-function checkAction(funcName) {
-    switch(funcName) {
-        case signUp:
-            signUp($("#email").val(), $("#password").val());
-            break;
-        case login:
-            login($("#email").val(), $("#password").val());
-            break;
-        case authenGoogle:
-            // code
-            break;
-        case logout:
-            // code
-            break;
-        case changePassword:
-            // code
-            break;
-        case forgotPassword:
-            // code
-            break;
-    }
-}
-
-// ฟังก์ชันเช็ก email ที่ user ให้มาว่าถูกใช้ในเว็บเราหรือยัง
-function existEmail(email) {
-    $.get("php file", {
-        user: email
-    }, function(userID) { return userID != '' ? true : false })
-}
-
-// ฟังก์ชันกำหนดค่า cookie
-function setCookie(userID) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = "userID=" + userID + ";" + expires + ";path=/";
-  }
-
-// ฟังก์ชันสร้างบัญชีผู้ใช้
-function signUp(email, password) {
-    if(!existEmail(email)) {
-        $.post("php file", {
-            user: email,
-            password: CryptoJS.MD5(password)
-        }, setCookie(userID), "text")
+    console.log(cookie('userID'));
+    if(typeof cookie('userID') !== 'undefined') {
+        $('#register').hide();
+        $('#signIn').hide();
+        $('#logOut').show();
     } else {
-        alert("Email: " + email + "is used already.");
+        $('#register').show();
+        $('#signIn').show();
+        $('#logOut').hide();
     }
-}
 
-// ฟังก์ชันลงชื่อเข้าใช้ระบบ
-function login(email, password) {
-    if(existEmail(email)) {
-        $.post("php file", {
-            user: email,
-            password: CryptoJS.MD5(password)
-        }, setCookie(userID), "text")
-    } else {
-        alert("Email: " + email + "is invalid!");
+    // ฟังก์ชันเช็ก email ที่ user ให้มาว่าถูกใช้ในเว็บเราหรือยัง
+    function existEmail(email) {
+        $.get("php file", {
+            user: email
+        }, function(userID) { return userID != '' ? true : false })
     }
-}
 
-// ฟังก์ชันยืนยันตัวตนด้วย google
-function authenGoogle() {
+    // ฟังก์ชันกำหนดค่า cookie
+    function setCookie(userID) {
+        cookie.empty().set('userID', userID, {
+            expires: exdays
+        });
+    }
 
-}
+    // ฟังก์ชันลงชื่อเข้าใช้ระบบ
+    $("#signIn_accept").click(function signIn() {
+        var email = $('#signIn_username').val();
+        var password = $('#signIn_password').val();
+        // if(existEmail(email)) {
+        //     $.get("php file", {
+        //         user: email,
+        //         password: CryptoJS.MD5(password)
+        //     }, setCookie(userID), "text")        
+        // } else {
+        //     alert("Email: " + email + "is used already.");
+        // }
+        setCookie(email);
+    // ต้องลิงก์ไปหน้าอื่น
+        location.reload(true);
+    })
 
-// ฟังก์ชันลงชื่อออกจากระบบ
+    // ฟังก์ชันสร้างบัญชีผู้ใช้
+    function register(email, password) {
+        // if(!existEmail(email)) {
+        //     $.post("php file", {
+        //         user: email,
+        //         password: CryptoJS.MD5(password)
+        //     }, setCookie(userID), "text")
+        // } else {
+        //     alert("Email: " + email + "is invalid!");
+        // }
+    }
+
+    // ฟังก์ชันยืนยันตัวตนด้วย google
+    function authenGoogle() {
+
+    }
+
+    // ฟังก์ชันลงชื่อออกจากระบบ
+    $('#logOut').click(function logOut() {
+        cookie.empty();
+        location.reload(true);
+    })
+})
